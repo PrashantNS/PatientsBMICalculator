3772be51941d4ae106f8ffb8b27bd410871a63a2
PrashantNS <chaudhry.prashant@gmail.com>
2021-06-19 19:04:39 +0530

    inital commit

--- src/main/java/com/bmi/bean/Patient.java
+++ src/main/java/com/bmi/bean/Patient.java
@@ -12,9 +12,17 @@ public class Patient {
 	Long height;
 	Double bmi;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
